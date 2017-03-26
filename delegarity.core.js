0c05b24017ec934755c55c9148a90f60d3e4a2a7
Bart Jolling <BartJolling@users.noreply.github.com>
2017-03-26 13:50:02 +0200

    Added autocomplete

--- delegarity.core.js
+++ delegarity.core.js
@@ -55,4 +55,26 @@
         return html;   
     }
 
+    delegarity.logXhrError = function (jqXHR, exception) {
+        var msg = '';
+
+        if (jqXHR.status === 0) {
+            msg = 'Not connect.\n Verify Network.';
+        } else if (jqXHR.status == 404) {
+            msg = 'Requested page not found. [404]';
+        } else if (jqXHR.status == 500) {
+            msg = 'Internal Server Error [500].';
+        } else if (exception === 'parsererror') {
+            msg = 'Requested JSON parse failed.';
+        } else if (exception === 'timeout') {
+            msg = 'Time out error.';
+        } else if (exception === 'abort') {
+            msg = 'Ajax request aborted.';
+        } else {
+            msg = 'Uncaught Error.\n' + jqXHR.responseText;
+        }
+
+        console.log("[delegarity] jQuery XHR Exception. Status: " + jqXHR.status + ". Exception: " + exception + ". Information: " + msg);
+    }
+
 })(window.delegarity = window.delegarity || {}, jQuery)
--- timeadmin.timesheet.js
+++ timeadmin.timesheet.js
@@ -8,8 +8,9 @@
 
         // private constants
         const ACTION = "timeadmin.timesheetbrowser";
-        const TARGET_URL = 'https://empower-sso.capgemini.com/niku/nu#action:' + ACTION;
+        const TARGET_URL = "https://empower-sso.capgemini.com/niku/nu#action:" + ACTION;
         const LS_KEY_MY_DELEGEES = ACTION + ".myDelegees";
+        const WAIT_HEADER = "https://empower-sso.capgemini.com/niku/ui/uitk/images/wait_header.gif"
 
         // public data members
         timeadmin.resourceNameElement = null;
@@ -33,19 +34,18 @@
             mainfilterRow.childNodes[1].width="20%";
             mainfilterRow.childNodes[2].width="40%";
 
-            delegarity.resourceNameElement = document.getElementsByName("ff_res_name")[0];
+            timeadmin.resourceNameElement = document.getElementsByName("ff_res_name")[0];
 
             var data = JSON.parse(localStorage.getItem(LS_KEY_MY_DELEGEES));
 
             var options = {
                 "delegeesOptions":delegarity.useTemplatebyId("delegeesSelectOptions-template", data)
             }
+            delegarityCell.style.verticalAlign = "top";
             delegarityCell.innerHTML = delegarity.useTemplatebyId("delegeesSelect-template", options);
 
-            //sync dropdown with actual value in 
-            debugger;
-
-            var val = (delegarity.resourceNameElement.value).trim();
+            //sync dropdown with actual value in input field
+            var val = (timeadmin.resourceNameElement.value).trim();
             var selectElement = document.getElementById("delegarity-delegees");
             var opts = selectElement.options;
             for(var opt, j = 0; opt = opts[j]; j++) {
@@ -54,8 +54,44 @@
                     break;
                 }
             }
-        }
 
+            //set autocomplete on existing resource name field
+            $(timeadmin.resourceNameElement).autocomplete({
+                source: function( request, response ) {
+                    $.ajax( {
+                        url: "https://empower-sso.capgemini.com/niku/odata/GetSuggestionsForLookup",
+                        method: "POST",
+                        dataType: "json",
+                        headers: {
+                            Accept: "application/json, text/javascript, */*; q=0.01",
+                            "Content-Type": "text/plain"
+                        },
+                        data: {
+                            "LookupType": "LOOKUP_SEC_RESMGR", 
+                            "SearchString": request.term, 
+                            "ParameterValues": "attributeCode=manager_id:objectCode=:partitionCode=NIKU.ROOT:viewType=filter:"
+                        },
+                        success: function( data ) {
+                            var results = $.map(data.d.lookupSuggestions.results, function(val, idx) {
+                                return {
+                                    id: val.keyAttribute,
+                                    label: val.labelAttribute,
+                                    value: val.labelAttribute
+                                }
+                            });
+                            response( results );
+                        },
+                        error: delegarity.logXhrError
+                    } );
+                },
+                minLength: 2,
+                messages: {
+                    noResults: '',
+                    results: function() {}
+                }                
+            })
+        }
+        
     })(delegarity.timeadmin = delegarity.timeadmin || {})
 
 })(window.delegarity = window.delegarity || {}, jQuery)

17d615ae07f14c84b812d7b1853e0f67d29e1697
Bart Jolling <BartJolling@users.noreply.github.com>
2017-04-04 14:23:27 +0200

    Remove custom template engine

--- delegarity.core.js
+++ delegarity.core.js
@@ -20,41 +20,6 @@
         }
     }; 
 
-    // Creates HTML based on adding data to the provided HTML template
-    delegarity.useTemplatebyId = function(templateId, data) {
-        var template = document.getElementById(templateId).innerHTML;
-        return delegarity.useTemplateByElement(template, data);
-    }
-
-    // Creates HTML based on adding data to the provided HTML template
-    delegarity.useTemplateByElement = function(template, data) {
-
-        if( template === null ||template === undefined)
-            throw "Cannot find template " + templateId;
-
-        // Replace the {{XXX}} with the corresponding property
-        function replaceWithData(data_bit) {
-            var html_snippet, prop, regex;
-            for (prop in data_bit) {
-                regex = new RegExp('{{' + prop + '}}', 'ig');
-                html_snippet = (html_snippet || template).replace(regex, data_bit[prop]);
-            }
-            return html_snippet;
-        }
-
-        var html = '';
-        
-        if( Array.isArray(data) ) {
-            // Go through each element in the array and add the properties to the template
-            for (var i = 0; i < data.length; i++) {
-                html += replaceWithData(data[i]);
-            }
-        } else {
-            html = replaceWithData(data);
-        }
-        return html;   
-    }
-
     delegarity.logXhrError = function (jqXHR, exception) {
         var msg = '';
 
